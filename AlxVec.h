56fc03fa8a62b07058039616488d506829afb2a8
AELLiiXX <mockus49@gmail.com>
2018-06-21 19:41:36 +0300

    Initial commit
    Features:
    Find fist in vector
    Find nth in vector

---
+++ AlxVec.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace avec
+{
+	/// Find first in vector
+	template <typename T>
+	int ffiv(T a, vector<T> vect)
+	{
+		for (int i = 0; i < vect.size() ; i++)
+		{
+			if (a == vect[i])
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/// Find first in vector for strings
+	int ffiv(string a, vector<string> vect)
+	{
+		for (int i = 0; i < vect.size(); i++)
+		{
+			if (a == vect[i])
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	/// Find nth in a vector
+	template <typename T>
+	int fniv(T a, vector<T> vect, int nth)
+	{
+		int iteration = 1;
+		for (int i = 0; i < vect.size(); i++)
+		{
+			if (a == vect[i])
+			{
+				if (iteration == nth) { return i; }
+				else { iteration++; }
+			}
+		}
+		return -1;
+	}
+	/// Find nth in a vector
+	int fniv(string a, vector<string> vect, int nth)
+	{
+		int iteration = 1;
+		for (int i = 0; i < vect.size(); i++)
+		{
+			if (a == vect[i])
+			{
+				if (iteration == nth) { return i; }
+				else { iteration++; }
+			}
+		}
+		return -1;
+	}
+}
